18bb9175112bc4413db1987d4856880d47b94b1d
Ricky Miller <ricky.miller@gmail.com>
2017-05-29 12:35:04 -0400

    Add format test

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 const test = require('tape')
 const platforms = require('./platforms')
+const format = require('./format')
 const getSub = (app, platform) => platforms(app, platform)[platform]
 
-test('urls should be correct', (t) => {
+test('urls should be correct', t => {
   t.plan(2)
   t.equal(
     getSub('twitter', 'ios').url,
@@ -12,4 +13,9 @@ test('urls should be correct', (t) => {
     getSub('mario', 'android').url,
     'https://www.google.com/#q=mario+android+app'
   )
+})
+
+test('format should return valid data uri', t => {
+  t.plan(1)
+  t.equal(format('image/jpeg', ['A', 'B']), 'data:image/jpeg;base64,QUI=')
 })
