b6654998e9a8f0a86de7fdb4437a3ce41a462ec0
Ricky Miller <ricky.miller@gmail.com>
2017-05-29 14:59:42 -0400

    Use site: search

--- platforms.js
+++ platforms.js
@@ -1,15 +1,22 @@
 const baseUrl = 'https://www.google.com/#q='
-module.exports = (app, platform) => ({
-  ios: {
-    url: `${baseUrl}${app}+${platform}+app`,
-    storeUrl: 'itunes.apple.com',
-    imgSelector: 'meta[itemprop]',
-    prop: 'content'
-  },
-  android: {
-    url: `${baseUrl}${app}+${platform}+app`,
-    storeUrl: 'play.google.com',
-    imgSelector: '.cover-image',
-    prop: 'src'
+module.exports = (app, platform) => {
+  const platforms = {
+    ios: {
+      storeUrl: 'itunes.apple.com',
+      imgSelector: 'meta[itemprop]',
+      prop: 'content'
+    },
+    android: {
+      storeUrl: 'play.google.com',
+      imgSelector: '.cover-image',
+      prop: 'src'
+    }
   }
-})
+
+  Object.keys(platforms).forEach((platform) => {
+    const sub = platforms[platform]
+    sub.url = `${baseUrl}site:${sub.storeUrl}+${app}+${platform}+app`
+  })
+
+  return platforms
+}
