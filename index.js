3b93a3dbd2caaab4cc94bd7a4e8132863caa268f
Ricky Miller <ricky.miller@gmail.com>
2017-05-31 00:48:26 -0400

    loadImage -> getImage

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('require-yaml')
 const platforms = require('./platforms.yml')
 const getUrl = require('./get-url')
-const loadImage = require('./load-image')
+const getImage = require('./get-image')
 const die = (reason) => console.log(reason) & process.exit(1)
 
 module.exports = (app, platform, callback) => {
@@ -17,7 +17,7 @@ module.exports = (app, platform, callback) => {
 
   return new Promise((resolve, reject) => {
     getUrl(_platform).then((url) => {
-      loadImage(url).then(callback || resolve)
+      getImage(url).then(callback || resolve)
     })
   })
 }
